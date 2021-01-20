8e4e6db037053b00da8a0f47454647eceae04fd6
facorreg <facorreg@gmail.com>
2021-01-21 00:00:14 +0100

    reset

--- stylelint.config.js
+++
@@ -1,8 +0,0 @@
-const sortOrderSmacss = require("stylelint-config-property-sort-order-smacss/generate");
-
-module.exports = {
-  plugins: ["stylelint-order"],
-  rules: {
-    "order/properties-order": [sortOrderSmacss({ emptyLineBefore: "always" })],
-  },
-};
