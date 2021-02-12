91f5b090835d1a10d437e9d9e9c6a8b6f26fa4f2
facorreg <facorreg@gmail.com>
2021-02-12 16:44:57 +0100

    updates on question elem

---
+++ src/hooks/ownDirKeyDownHandler/index.jsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+
+import { isSSG } from '@utils';
+
+const ownKeyDownHandler = (readingsByWord) => {
+  const [mainIndex, setMainIndex] = useState(0);
+  const [secondaryIndex, setSecondaryIndex] = useState(0);
+
+  const onKeyDownHandler = (event) => {
+    const LEFT = 37;
+    const UP = 38;
+    const RIGHT = 39;
+    const DOWN = 40;
+
+    const updatemainIndex = (value) => {
+      let newVal = mainIndex + value;
+
+      setSecondaryIndex(0);
+
+      if (newVal < 0) {
+        newVal = readingsByWord.length - 1;
+      } else if (newVal === readingsByWord.length) {
+        newVal = 0;
+      }
+
+      setMainIndex(newVal);
+      return newVal;
+    };
+
+    const updatesecondaryIndex = (value) => {
+      const newVal = secondaryIndex + value;
+
+      if (newVal < 0) {
+        const newIndex = updatemainIndex(-1);
+        setSecondaryIndex(readingsByWord[newIndex] - 1);
+        // eslint-disable-next-line react/prop-types
+      } else if (newVal === readingsByWord[mainIndex]) {
+        updatemainIndex(1);
+      } else {
+        setSecondaryIndex(newVal);
+      }
+    };
+
+    if (event.keyCode === RIGHT || event.keyCode === LEFT) {
+      updatemainIndex(event.keyCode === RIGHT ? 1 : -1);
+    } else if (event.keyCode === UP || event.keyCode === DOWN) {
+      updatesecondaryIndex(event.keyCode === UP ? 1 : -1);
+    }
+  };
+
+  useEffect(() => {
+    if (!isSSG()) {
+      window.addEventListener('keydown', onKeyDownHandler);
+    }
+
+    return () => {
+      if (!isSSG()) window.removeEventListener('keydown', onKeyDownHandler);
+    };
+  });
+
+  return [mainIndex, secondaryIndex];
+};
+
+export default ownKeyDownHandler;
--- src/pages/word/question/[word]/index.jsx
+++ src/pages/word/question/[word]/index.jsx
@@ -1,72 +1,32 @@
 import axios from 'axios';
 import PropTypes from 'proptypes';
-import { useState, useEffect } from 'react';
 
 import Furigana from '@components/Furigana';
+import ownKeyDownHandler from '@hooks/ownDirKeyDownHandler';
 import { kanjiPropTypes } from '@propTypes';
-import { isSSG } from '@utils';
+import styles from '@styles/Question.module.scss';
 
 // eslint-disable-next-line sonarjs/cognitive-complexity
 const WordQuestion = (props) => {
   const { words } = props;
-  const [wordIndex, setWordIndex] = useState(0);
-  const [japaneseIndex, setJapaneseIndex] = useState(0);
-
-  const onKeyUpHandler = (event) => {
-    const LEFT = 37;
-    const UP = 38;
-    const RIGHT = 39;
-    const DOWN = 40;
-
-    const updateWordIndex = (value) => {
-      let newVal = wordIndex + value;
-
-      setJapaneseIndex(0);
-
-      if (newVal < 0) {
-        newVal = words.length - 1;
-      } else if (newVal === words.length) {
-        newVal = 0;
-      }
-
-      setWordIndex(newVal);
-      return newVal;
-    };
-
-    const updateJapaneseIndex = (value) => {
-      const newVal = japaneseIndex + value;
-
-      if (newVal < 0) {
-        const newIndex = updateWordIndex(-1);
-        setJapaneseIndex(words[newIndex].japanese.length - 1);
-        // eslint-disable-next-line react/prop-types
-      } else if (newVal === words[wordIndex].japanese?.length) {
-        updateWordIndex(1);
-      } else {
-        setJapaneseIndex(newVal);
-      }
-    };
-
-    if (event.keyCode === RIGHT || event.keyCode === LEFT) {
-      updateWordIndex(event.keyCode === RIGHT ? 1 : -1);
-    } else if (event.keyCode === UP || event.keyCode === DOWN) {
-      updateJapaneseIndex(event.keyCode === UP ? 1 : -1);
-    }
-  };
-
-  useEffect(() => {
-    if (!isSSG()) {
-      window.addEventListener('keyup', onKeyUpHandler);
-    }
-
-    return () => {
-      if (!isSSG()) window.removeEventListener('keyup', onKeyUpHandler);
-    };
-  });
 
   if (!words) return null;
+  const readingsByWord = words.map(({ japanese }) => japanese.length);
 
-  return <Furigana {...(words[wordIndex]?.japanese[japaneseIndex] || {})} cName="question" />;
+  const [mainIndex, secondaryIndex] = ownKeyDownHandler(readingsByWord);
+
+  return (
+    <div className={styles.questionContainer}>
+      <Furigana {...(words[mainIndex]?.japanese[secondaryIndex] || {})} cName="question" />
+      <br />
+      <div>
+        word: {mainIndex + 1} / {words.length}
+      </div>
+      <div>
+        reading: {secondaryIndex + 1} / {words[mainIndex].japanese.length}
+      </div>
+    </div>
+  );
 };
 
 export const getStaticPaths = async () => {
